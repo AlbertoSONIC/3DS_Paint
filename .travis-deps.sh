a95c8d4cdd2178c06292d732bbbdeb1d8cd4784f
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-10-20 16:00:05 +0200

    Create .travis-deps.sh

---
+++ .travis-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+set -x
+
+# Build and install devkitARM + ctrulib
+wget http://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitARMupdate.pl
+git clone https://github.com/smealum/ctrulib.git
+sudo perl devkitARMupdate.pl
+
+# Get latest ctrulib and overwrite bundled one
+cd ctrulib/libctru && make ; cd -
+sudo cp -rf ctrulib/libctru/ ${DEVKITPRO}
